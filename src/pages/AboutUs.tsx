15f357d57252070e1c76bb70676622077dccfe65
AHMED FARES <ahmedfaresss416@gmail.com>
2024-11-25 20:20:07 +0200

    add text for aboutUs Page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,7 +1,13 @@
+import { Container } from "react-bootstrap";
 
 
 export default function AboutUs() {
   return (
-    <div className="fs-2 text-center mt-5">Website under Development, ✅⛔</div>
+    <Container fluid={"md"}>
+      <div className="fs-4 pb-5 text-uppercase d-flex text-center justify-content-center align-align-items-center flex-column  vh-100">
+      <h1 className="mb-5">Website under Development</h1>
+      <p className=" mx-auto">more features will added as soon as the backend developer implemented more endpoints such as integration payment, also some enhancement for all website will added soon .</p>
+    </div>
+    </Container>
   )
 }
